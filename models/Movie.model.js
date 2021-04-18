17f8985cdfc1236f7b2b04776a303f1b098ac881
Raquel R. Garcia <raquelrgarc@hotmail.com>
2021-04-18 17:55:32 +0200

    it. 3 and 4 done

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -7,7 +7,11 @@ const movieSchema = new Schema({
     stars: {
         type: [String],
     },
-    image: String,
+    image: {
+        type: String,
+        default: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyMTk2MTQ3Ml5BMl5BanBnXkFtZTgwNDQ2ODE0NDM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    },
+    
     description: String,
     showtimes: {
         type: [String],
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-//requiring the movies model 
+//requiring the movies model
 
-const MoviesCollection = require("../models/Movie.model.js")
+const MoviesCollection = require("../models/Movie.model.js");
 
 //---READ ROUTES---//
-router.get("/movies", (req, res, next) =>{
-//To show the elements, we need to get them first from the DB 
-    console.log("routes working")
-    MoviesCollection.find() //this is a promise, we need to consume it 
-    .then((allMovies)=>{
-        res.render("movies.hbs", {allMovies} )
-        console.log(allMovies)
+router.get("/movies", (req, res, next) => {
+  //To show the elements, we need to get them first from the DB
+  console.log("routes working");
+  MoviesCollection.find() //this is a promise, we need to consume it
+    .then((allMovies) => {
+      res.render("movies.hbs", { allMovies });
+      console.log(allMovies);
+    });
+});
+
+router.get("/seemore/:id", (req, res, next) => {
+  const { id } = req.params;
+  MoviesCollection.findById(id) //this is a promise
+    .then((data) => {
+      res.render("see.more.hbs", { data });
     })
 
-})
+    .catch(() => {});
+});
 
 module.exports = router;
